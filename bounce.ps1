365e636a24e1846a872d81e6d71ff3efb02a8599
Rebecca Turner <637275@gmail.com>
2016-11-10 15:38:32 -0500

    init commit

---
+++ bounce.ps1
@@ -0,0 +1,51 @@
+function bounce {
+	Param(
+		[String]$Push
+	)
+
+	#don't discriminate between PS natives and git natives
+	$Push = $Push.ToLower()
+
+	#assume we want to pull
+	#maybe ill put in a setting for this later
+	$Type = "local"
+	#cosmetic
+	$Style = "Pulling remote files to local directory."
+
+	#"push" or "up"
+	#otherwise "pull"/"down" is implicit
+	If(
+		$Push.StartsWith("pus") -or
+		$Push.StartsWith("u") -or
+		$Push.StartsWith("↑")
+	) {
+		$Type = "remote"
+		$Style = "Pushing local files to remote directory."
+	} ElseIf($Push.StartsWith("b")) {
+		$Type = "both"
+		$Style = "Synchronizing local and remote directories to match."
+	}
+
+	#get the bounce file
+	#first line is the remote directory, second is the key
+	#all other lines are ignored
+	$bf = Get-Content ".\bounce.dir"
+	$Dir = $bf[0]
+	$Key = $bf[1]
+
+	#create a temp file but be quiet about it
+	New-Item ".\bounce.scp~" | Out-Null
+
+	"open sftp://user@becca.ooo/ -hostkey=`"$Key`"",
+	"lcd $(pwd)",
+	"cd $Dir",
+	"echo $Style",
+	"sync $Type -delete -filemask=`"| *swp; *swo; *~`" .\ ./",
+	"exit" -join "`n" | Out-File ".\bounce.scp~"
+
+	#sync it
+	winscp /ini=nul /script=".\bounce.scp~"
+
+	#get rid of the temp file
+	Remove-Item ".\bounce.scp~"
+}
